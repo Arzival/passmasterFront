699b5b570ca5c53c0ff9b241619fe828dcbcc8a5
Armando <arzival.sistemas@gmail.com>
2024-10-28 23:05:25 -0600

    Update: Instalando y configurando tailwind para usarlo en vez de css

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,22 +1,18 @@
 import { Link } from 'react-router-dom';
-//import '../../styles/nav.css';
 
 const Nav = () => {
   return (
-    <nav className="nav-bar">
-      <ul className="nav-list">
+    <nav className="bg-white bg-opacity-90 backdrop-blur-md p-3 rounded-md shadow-lg">
+      <ul className="list-none m-0 p-0 flex items-center w-full gap-4">
         <li>
-          <Link to="/home">Inicio</Link>
+          <Link to="/home" className="text-black font-bold">PassMaster</Link>
         </li>
-        <li>
-          <Link to="/roadmap">Roadmap</Link>
-        </li>
-        <div className="auth-links"> {/* Agrupando las opciones de autenticación */}
+        <div className="ml-auto flex gap-4">
           <li>
-            <Link to="/login">Login</Link>
+            <Link to="/login" className="text-black font-bold">Login</Link>
           </li>
           <li>
-            <Link to="/register">Registro</Link>
+            <Link to="/register" className="text-black font-bold">Registro</Link>
           </li>
         </div>
       </ul>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
 const Home = () => {
-  return <h1>Hola mundo desde la página Home</h1>;
+  return (
+    <main className="flex-1">
+    <section className="w-full py-12 md:py-24 lg:py-32">
+      <div className="container px-4 md:px-6">
+        <div className="grid gap-6 lg:grid-cols-2 lg:gap-12 xl:grid-cols-2">
+          <div className="flex flex-col justify-center space-y-4">
+            <div className="space-y-2">
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+                Genera y almacena contraseñas de manera segura
+              </h1>
+              <p className="max-w-[600px] text-zinc-500 md:text-xl dark:text-zinc-400">
+                Nuestra plataforma te ofrece una solución confiable para crear y guardar tus contraseñas. Mantén tus cuentas protegidas con nuestro avanzado sistema de seguridad.
+              </p>
+            </div>
+            <div className="flex flex-col gap-2 min-[400px]:flex-row">
+              <button className="btn btn-primary">Regístrate</button>
+             
+            </div>
+          </div>
+          <div className="flex items-center justify-center">
+            <span>aqui va la imagen</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+  );
 };
 
 export default Home;
